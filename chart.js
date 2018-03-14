5efbfebab375056210f07080c72bea85c1a5feae
Argyros Vasilis <p15argy@ionio.gr>
2018-03-14 02:27:20 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -158,13 +158,13 @@ function fundsType() {
 }
 
 
-//function amountType() {
-	//force.gravity(0)
-		//.friction(0.75)
-//.charge(function(d) { return -Math.pow(d.radius, 2.0) / 3; })
-		//.on("tick", amount)
-	//	.start();
-//}
+function amountType() {
+	force.gravity(0)
+		.friction(0.75)
+.charge(function(d) { return -Math.pow(d.radius, 2.0) / 3; })
+		.on("tick", amount)
+		.start();
+}
 
 
 function parties(e) {
